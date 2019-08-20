ffadcf7f8cddb5948037467e24c5224c4f19410d
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2019-08-20 14:01:36 -0700

    Update igvR_Function.R

--- explore/ctcf/igvR_Function.R
+++ explore/ctcf/igvR_Function.R
@@ -1,9 +1,9 @@
 runTests<- function()
 {
-  test_igvBam()
+  testIgvBam()
 }
 
-igvBam <- function(FileBam, tf, chromosome, narrowPeakFilefromMACS2, broadPeakFilefromMACS2)
+igvBam <- function(fileBam, tf, chromosome, narrowPeakFilefromMACS2, broadPeakFilefromMACS2)
 {
 
   library(RUnit)
@@ -11,68 +11,68 @@ igvBam <- function(FileBam, tf, chromosome, narrowPeakFilefromMACS2, broadPeakFi
   library(VariantAnnotation)
   
   
-  idx = indexBam(FileBam)
+  idx = indexBam(fileBam)
   igv <- igvR() #connects to browser
   setGenome(igv, "hg19")
   showGenomicRegion(igv, tf)
   
   unzippedTables <- lapply (c(narrowPeakFilefromMACS2,broadPeakFilefromMACS2), read.table)
   tables <- lapply(unzippedTables,as.data.frame)
-  tbl.Np <- tables[[1]]
-  tbl.Bp <- tables[[2]]
-  colnames(tbl.Np) <- c("chrom", "start", "end", "name", "score","width")
-  colnames(tbl.Bp) <- c("chrom", "start", "end", "name", "score", "width")
+  tbl.np <- tables[[1]]
+  tbl.bp <- tables[[2]]
+  colnames(tbl.np) <- c("chrom", "start", "end", "name", "score","width")
+  colnames(tbl.bp) <- c("chrom", "start", "end", "name", "score", "width")
   
   
-  tbl.Bp.Chrom <- tbl.bp[(grep(chromosome, tbl.Bp$chrom)[1]):((grep(chromosome, tbl.Bp$chrom)[1])+(length(grep(chromosome, tbl.Bp$chrom)))),]
-  tbl.BestBroad.Chrom <-subset(tbl.Bp.Chrom, score == max(tbl.bp.chrom$score))
+  tbl.bp.chrom <- tbl.bp[(grep(chromosome, tbl.bp$chrom)[1]):((grep(chromosome, tbl.bp$chrom)[1])+(length(grep(chromosome, tbl.bp$chrom)))),]
+  tbl.bestBroad.chrom <-subset(tbl.bp.chrom, score == max(tbl.bp.chrom$score))
   
   
-  start.loc.Specific= tbl.bestBroad.Chrom [1,'start']
-  start.loc=  start.loc.Specific-5000
-  end.loc.Specific= tbl.bestBroad.Chrom [1,'end']
-  end.loc=end.loc.Specific+5000
+  start.loc.specific= tbl.bestBroad.chrom [1,'start']
+  start.loc= start.loc.specific-5000
+  end.loc.specific= tbl.bestBroad.chrom [1,'end']
+  end.loc=end.loc.specific+5000
     
-  tbl.Bp.Chrom.Sub <- subset(tbl.bp.chrom, start.loc < start & end> end.loc)
+  tbl.bp.chrom.sub <- subset(tbl.bp.chrom, start.loc < start & end> end.loc)
   
-  tbl.Np.Chrom <- tbl.Np[(grep(chromosome, tbl.Np$chrom)[1]): ((grep(chromosome, tbl.Np$chrom)[1])+(length(grep(chromosome, tbl.Np$chrom)))),]
-  tbl.Np.Chrom.Sub <- subset(tbl.Np.Chrom,  start.loc < start & end> end.loc)
+  tbl.np.chrom <- tbl.np[(grep(chromosome, tbl.np$chrom)[1]): ((grep(chromosome, tbl.np$chrom)[1])+(length(grep(chromosome, tbl.np$chrom)))),]
+  tbl.np.chrom.sub <- subset(tbl.np.chrom,  start.loc < start & end> end.loc)
 
-  NarrowTrack <- DataFrameAnnotationTrack("NarrowTrack", tbl.Np.Chrom.Sub, color="red", displayMode="SQUISHED")
-  BroadTrack <- DataFrameAnnotationTrack("BroadTrack", tbl.Bp.Chrom.Sub, color="green", displayMode="SQUISHED")
-  displayTrack(igv, NarrowTrack)
-  displayTrack(igv, BroadTrack)
+  narrowTrack <- DataFrameAnnotationTrack("narrowTrack", tbl.np.chrom.sub, color="red", displayMode="SQUISHED")
+  broadTrack <- DataFrameAnnotationTrack("broadTrack", tbl.bp.chrom.sub, color="green", displayMode="SQUISHED")
+  displayTrack(igv, narrowTrack)
+  displayTrack(igv, broadTrack)
   
   
   
   which <- GRanges(seqnames = chromosome, ranges = IRanges(start.loc, end.loc))
   param <- ScanBamParam(which=which)
   
-  x <- readGAlignments(FileBam, use.names=TRUE, param=param)
+  x <- readGAlignments(fileBam, use.names=TRUE, param=param)
   track <- GenomicAlignmentTrack("DNAse", x)
   displayTrack(igv, track)
 }
 
 #test
-test_igvBam <- function()
+testIgvBam <- function()
 {
   message(sprintf("--- test_igvBam"))
   
-  FileBam="GSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam"
-  checkTrue(file.exists(FileBam))
+  fileBam="GSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam"
+  checkTrue(file.exists(fileBam))
   
   tf="CTCF"
   chromosome= "chr11"
   narrowPeakFilefromMACS2= "ctcf__peaks.narrowPeak"
   broadPeakFilefromMACS2= "ctcf__peaks.broadPeak"
   
-  igvBam(FileBam, tf, chromosome, narrowPeakFilefromMACS2, broadPeakFilefromMACS2)
+  igvBam(fileBam, tf, chromosome, narrowPeakFilefromMACS2, broadPeakFilefromMACS2)
   
-  checkTrue(c("igvAnnotationTrack") %in% is(NarrowpTrack))
-  checkTrue(c("igvAnnotationTrack") %in% is(BroadpTrack))
+  checkTrue(c("igvAnnotationTrack") %in% is(narrowTrack))
+  checkTrue(c("igvAnnotationTrack") %in% is(broadTrack))
 }
   
-if(!interative()) #you can run tests from terminal
+if(!interactive()) #you can run tests from terminal
   runTests()
 
 
