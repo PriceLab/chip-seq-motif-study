3e2d53b669bdff051592e0d557c7690fbfeac7fe
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2019-08-23 23:16:19 -0700

    Update sliceBamFunction.R

--- explore/ctcf/sliceBamFunction.R
+++ explore/ctcf/sliceBamFunction.R
@@ -1,20 +1,49 @@
-sliceBamFile <- function(existingBamFileNam, chrom, start, end, newBamFileName)
+library(Rsamtools)
+library(RUnit)
+library(org.Hs.eg.db)
+
+#----------------------------------------------------------------------------------------------------
+runTests <- function()
 {
-  library(Rsamtools)
+  test_sliceBamFile()
+  
+} # runTests
+#----------------------------------------------------------------------------------------------------
+sliceBamFile <- function(bamFile, chrom, start=NA, end=NA, newBamFileName)
+{
+  if(is.na(start)){
+    start<- 1
+  }
+  
+  if(is.na(end)){
+    truncated.chromosome.name <- as.character(sub("chr", "", chrom))
+    end <- org.Hs.egCHRLENGTHS[[truncated.chromosome.name]]
+  }
   which <- GRanges(seqnames = chrom, ranges = IRanges(start, end))
   what <- c("rname", "strand", "pos", "qwidth", "seq")
   param <- ScanBamParam(which = which, what=what)
-  bamFile<- sprintf("~/github/igvR/inst/extdata/%s", existingBamFileNam)
-  bam <- scanBam(bamFile, param=param)
-  filterBam(bamFile, newBamFileName, param=param)
-}
-
-#unit test
-testSliceBamFile<- function()
-{
-  message(sprintf("--- test_igvBam"))
   
-  sliceBamFile ("GSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam","chr19", 1, 59128983, "temp1.bam")
-  checkTrue(file.exists("GSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam"))
-  checkTrue(file.exists("temp1.bam"))
-  }
+  bam <- scanBam(bamFile, param=param)
+  
+  filterBam(bamFile, newBamFileName, param=param)
+  
+} # sliceBamFile
+#----------------------------------------------------------------------------------------------------
+test_sliceBamFile<- function()
+{
+  message(sprintf("--- test_sliceBamFile"))
+  
+  bamFile <- "GSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam"
+  indexFile <- sprintf("%s.bai", bamFile)
+  checkTrue(file.exists(bamFile))
+  checkTrue(file.exists(indexFile))
+  
+  sliced.file <- "chr19-ctcf.bam"
+  sliceBamFile(bamFile,"chr19", 1, 59128983, sliced.file)
+  checkTrue(file.exists(sliced.file))
+  
+} # test_sliceBamFile
+#----------------------------------------------------------------------------------------------------
+if(!interactive())
+  runTests()
+
