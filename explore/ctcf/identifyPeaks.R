039bd81f198c34945ddb022389db957f3bd23d8a
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2019-08-26 15:51:49 -0700

    Update identifyPeaks.R

--- explore/ctcf/identifyPeaks.R
+++ explore/ctcf/identifyPeaks.R
@@ -1,11 +1,16 @@
-identifyPeaks<- function(bamFile, chrom, start.loc, end.loc, newBamFileName)
+identifyPeaks<- function(bamFile, chrom, start, end)
 {
+  library(Rsamtools)
+  library(RUnit)
+  library(org.Hs.eg.db)
+  
+  newBamFileName<-sprintf(("slice%s"),bamFile)
   if (exists("sliceBamFile")){
-    sliceBamFile(bamFile, chrom, start.loc, end.loc, newBamFileName)
-    } else {
-      source("sliceBamFile.R")
-      sliceBamFile(bamFile, chrom, start.loc, end.loc, newBamFileName)
-      }
+    sliceBamFile(bamFile, chrom, start, end, newBamFileName)
+  } else {
+    source("sliceBamFile.R")
+    sliceBamFile(bamFile, chrom, start, end, newBamFileName)
+  }
   truncated.new.bamFile.name <- as.character(sub(".bam","", newBamFileName))
   system(sprintf("docker run -v $PWD:/data/ fooliu/macs2 callpeak -t /data/%s -c --broad -g hs --broad-cutoff 0.1 -n %s --outdir /data/", newBamFileName,truncated.new.bamFile.name))
   system(sprintf("docker run -v $PWD:/data/ fooliu/macs2 callpeak -t /data/%s -c  -n %s --outdir /data/", newBamFileName,truncated.new.bamFile.name))
@@ -14,21 +19,10 @@ identifyPeaks<- function(bamFile, chrom, start.loc, end.loc, newBamFileName)
   tables <- lapply(unzippedTables,as.data.frame)
   tbl.narrow <- tables[[1]]
   tbl.broad <- tables[[2]]
-  colnames(tbl.narrow) <- c("chrom", "start", "end", "name", "score","width")
-  colnames(tbl.broad) <- c("chrom", "start", "end", "name", "score", "width")
-  tbl.broad.chrom <- tbl.broad[(grep(chrom, tbl.broad$chrom)[1]):((grep(chrom, tbl.broad$chrom)[1])+(length(grep(chrom, tbl.broad$chrom)))),]
-  tbl.broad.chrom.sub <- subset(tbl.broad.chrom, start.loc < start & end> end.loc)
+  colnames(tbl.narrow) <- c("chrom", "start.loc", "end.loc", "name", "score","width")
+  colnames(tbl.broad) <- c("chrom", "start.loc", "end.loc", "name", "score", "width")
   
-  tbl.narrow.chrom <- tbl.narrow[(grep(chrom, tbl.narrow$chrom)[1]): ((grep(chrom, tbl.narrow$chrom)[1])+(length(grep(chrom, tbl.narrow$chrom)))),]
-  tbl.narrow.chrom.sub <- subset(tbl.narrow.chrom,  start.loc < start & end> end.loc)
-  
-  if(is.na(end.loc)){
-    tbls<- list(tbl.narrow.chrom,tbl.broad.chrom)
-    names(tbls)<- c("narrow","broad")
-    return(tbls) 
-    } else { 
-      tbls<- list(tbl.narrow.chrom.sub,tbl.broad.chrom.sub)
-      names(tbls)<- c("narrow","broad")
-      return(tbls) 
-    }
-  }
+  tbls<- list(tbl.narrow, tbl.broad)
+  names(tbls)<- c("narrow","broad")
+  return(tbls) 
+}
