9bc51353ce9a39b134659cfbde75a9902fc2ed4c
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2019-08-27 11:55:15 -0700

    Update test_identifyPeaks.R

--- explore/ctcf/test_identifyPeaks.R
+++ explore/ctcf/test_identifyPeaks.R
@@ -4,29 +4,28 @@ library(RUnit)
 #------------------------------------------------------------------------------------------------------------------------
 runTests <- function()
 {
-   test_identifyPeaks()
-
+  test_identifyPeaks()
+  
 } # runTests
 #------------------------------------------------------------------------------------------------------------------------
 test_identifyPeaks <- function()
 {
-   bamFile <- "GSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam"
-
-   tbls <- identifyPeaks(bamFile, chrom="chr19", start=10000000, end=51190311)
-   checkTrue(is.list(tbls))
-   checkTrue(all(c("narrow", "broad") %in% names(tbls)))
-
-   tbl.narrow <- tbls$narrow
-   tbl.broad <- tbls$broad
-
-   checkTrue(is.data.frame(tbl.narrow))
-   checkTrue(is.data.frame(tbl.broad))
-
-   checkTrue(nrow(tbl.narrow) > 0)
-   checkTrue(nrow(tbl.broad) > 0)
-
-     # many more checks to come
-
+  bamFile <- "sliceGSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam"
+  
+  tbls <- identifyPeaks(bamFile, chrom="chr19", start=53160025, end=56170311)
+  checkTrue(is.list(tbls))
+  checkTrue(all(c("narrow", "broad") %in% names(tbls)))
+  
+  tbl.narrow <- tbls$narrow
+  tbl.broad <- tbls$broad
+  
+  checkTrue(is.data.frame(tbl.narrow))
+  checkTrue(is.data.frame(tbl.broad))
+  
+  checkTrue(nrow(tbl.narrow) > 0)
+  checkTrue(nrow(tbl.broad) > 0)
+  
+  # many more checks to come
+  
 } # test_identifyPeaks
 #------------------------------------------------------------------------------------------------------------------------
-
