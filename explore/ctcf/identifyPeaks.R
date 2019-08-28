15e1d14000af2f141c26961396668441f07a8e0d
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2019-08-28 11:58:35 -0700

    Update identifyPeaks.R

--- explore/ctcf/identifyPeaks.R
+++ explore/ctcf/identifyPeaks.R
@@ -5,42 +5,63 @@ library(RUnit)
 library(org.Hs.eg.db)
 #------------------------------------------------------------------------------------------------------------------------
 if(!exists("sliceBamFile"))
-   source("sliceBamFile.R")
+  source("sliceBamFile.R")
 #------------------------------------------------------------------------------------------------------------------------
 identifyPeaks<- function(bamFile, chrom, start, end)
 {
-
-#  if (exists("sliceBamFile")){
-#    sliceBamFile(bamFile, chrom, start, end, newBamFileName)
-#  } else {
-#    source("sliceBamFile.R")
-#    sliceBamFile(bamFile, chrom, start, end, newBamFileName)
-#  }
-
-  browser()
-  newBamFileName<-sprintf(("slice%s"), bamFile)
-
-  sliceBamFile(bamFile, chrom, start, end, newBamFileName)
-  truncated.new.bamFile.name <- as.character(sub(".bam","", newBamFileName))
-
+  
+  truncated.bamFile.name <- as.character(sub(".bam","", bamFile))
+  
   broadpeaks.cmd <- sprintf("docker run -v $PWD:/data/ fooliu/macs2 callpeak -t /data/%s -c --broad -g hs --broad-cutoff 0.1 -n %s --outdir /data/",
-                            newBamFileName,truncated.new.bamFile.name)
-
+                            bamFile,truncated.bamFile.name)
+  
   system(broadpeaks.cmd)
   narrowpeaks.cmd <- sprintf("docker run -v $PWD:/data/ fooliu/macs2 callpeak -t /data/%s -c  -n %s --outdir /data/",
-                             newBamFileName,truncated.new.bamFile.name)
+                             bamFile,truncated.bamFile.name)
   system(narrowpeaks.cmd)
-
-  unzippedTables<- lapply(sprintf(c("%s_peaks.narrowPeak","%s_peaks.broadPeak"),truncated.new.bamFile.name), read.table)
+  
+  unzippedTables<- lapply(sprintf(c("%s_peaks.narrowPeak","%s_peaks.broadPeak"),truncated.bamFile.name), read.table)
   tables <- lapply(unzippedTables,as.data.frame)
   tbl.narrow <- tables[[1]]
   tbl.broad <- tables[[2]]
   colnames(tbl.narrow) <- c("chrom", "start.loc", "end.loc", "name", "score","width")
   colnames(tbl.broad) <- c("chrom", "start.loc", "end.loc", "name", "score", "width")
-
+  
   tbls<- list(tbl.narrow, tbl.broad)
   names(tbls)<- c("narrow","broad")
   return(tbls)
-
+  
 } # identifyPeaks
+#------------------
+
+# test_identifyPeaks.R
+source("identifyPeaks.R")
+library(RUnit)
 #------------------------------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+  test_identifyPeaks()
+  
+} # runTests
+#------------------------------------------------------------------------------------------------------------------------
+test_identifyPeaks <- function()
+{
+  bamFile <- "sliceGSM749704_hg19_wgEncodeUwTfbsGm12878CtcfStdAlnRep1.bam"
+  
+  tbls <- identifyPeaks(bamFile, chrom="chr19", start=53160025, end=56170311)
+  checkTrue(is.list(tbls))
+  checkTrue(all(c("narrow", "broad") %in% names(tbls)))
+  
+  tbl.narrow <- tbls$narrow
+  tbl.broad <- tbls$broad
+  
+  checkTrue(is.data.frame(tbl.narrow))
+  checkTrue(is.data.frame(tbl.broad))
+  
+  checkTrue(nrow(tbl.narrow) > 0)
+  checkTrue(nrow(tbl.broad) > 0)
+  
+  # many more checks to come
+  
+} # test_identifyPeaks
+#-----------------------
